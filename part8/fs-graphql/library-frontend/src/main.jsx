f0587d88e0afd60f1a676c1d9f0c984610c29af7
Rodrigo Gomez Fernandez <108586272+godridrigo233@users.noreply.github.com>
2026-07-14 22:07:47 +0000

    cambios

--- part8/fs-graphql/library-frontend/src/main.jsx
+++ part8/fs-graphql/library-frontend/src/main.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
-// Importaciones de Apollo
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
 
-// 1. Definir el enlace
 const httpLink = createHttpLink({
-  uri: 'https://cautious-halibut-5j4g55rv694fv7pj-4000.app.github.dev/',
+  uri: 'http://localhost:4000',
 })
 
-// 2. Crear el cliente
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: httpLink
 })
-
-// 3. Envolver <App /> con ApolloProvider
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
     <App />
